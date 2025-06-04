b930a5337a5966b39f3df66c71b464eee2345b25
glbert-does <tachydromos@hotmail.com>
2025-06-04 11:52:05 +0200

    fix faulty rebase

--- heltour/settings_testing.py
+++ heltour/settings_testing.py
@@ -24,4 +24,35 @@ PASSWORD_HASHERS = [
 #        'NAME': ':memory:',
 #    }
 #}
->>>>>>> add hasher, potentially use different database for tests
+
+INSTALLED_APPS = [
+    'cacheops',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'heltour.%s' % HELTOUR_APP,
+    'reversion',
+    'bootstrap3',
+    'ckeditor',
+    'ckeditor_uploader',
+    'django_comments',
+    'heltour.comments',
+    'static_precompiler',
+    'impersonate',
+]
+
+# remove some middleware for tests
+MIDDLEWARE = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+]
+
+ALLOWED_HOSTS = [
+    'testserver',
+]
